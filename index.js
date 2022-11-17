0427ed7bd1bb06be9ffd70351e19008fba8851b8
marshall <mhamad.marshall@gmail.com>
2022-11-17 14:00:47 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+
+  const testedValues = [];
+  for (let i = 0; i < array.length; i++) {
+    const currentValue = array[i];
+    if (testedValues.includes(target - currentValue)) {
+      return true;
+    }
+    testedValues.push(currentValue);
+  }
+  return false;
 }
 
-/* 
+/*
   Write the Big O time complexity of your function here
+  O(n)
 */
 
-/* 
+/*
   Add your pseudocode here
+  1. Create an empty array to store the tested values inside
+  2. create a loop
+  3. create a variable thet holds the value of the iteration
+  4. make a condition that checks if the target minus is in the tested values array
+  5. If it do that will return true
+  6. If not push values in testedValues
+  7. Return false
 */
 
 /*
   Add written explanation of your solution here
+  we go through the array once and check if the tested values includes the has the tageted value minus current value in array and return true if it does
 */
 
 // You can run `node index.js` to view these console logs
